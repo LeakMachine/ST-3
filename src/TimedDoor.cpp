05c0893acd47b72f3276b960a84cfdabd5a89d32
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2024-04-07 17:24:10 +0300

    fix 2

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -3,3 +3,47 @@
 #include "TimedDoor.h"
 #include <stdexcept>
 #include <iostream>
+
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor& door) : door(door) {}
+
+void DoorTimerAdapter::Timeout() {
+  door->throwState();
+}
+
+TimedDoor::TimedDoor(int timeout) {
+  iTimeout = timeout;
+  isOpened = false;
+}
+
+bool TimedDoor::isDoorOpened() {
+  return isOpened;
+}
+
+void TimedDoor::unlock() {
+  isOpened = true;
+  Timer timer;
+  timer.tregister(getTimeOut(), adapter);
+}
+
+void TimedDoor::lock() {
+  isOpened = false;
+}
+
+int TimedDoor::getTimeOut() {
+  return iTimeout;
+}
+
+void TimedDoor::throwState() {
+  if (isOpened == true) {
+       throw std::runtime_error("Door timeout");
+  }
+}
+
+void Timer::sleep(int time) {
+  for (int i = time; i > 0; i--) {}
+}
+
+void Timer::tregister(int time, TimerClient* client) {
+  sleep(time);
+  client->Timeout();
+}
